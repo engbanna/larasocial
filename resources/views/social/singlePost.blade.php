e0965ba0dbc888372ef35fe102a41bb326f13fb1
zain <eng.banna1@gmail.com>
2016-09-16 22:17:36 +0300

    test each_post_can_have_comments

--- resources/views/social/singlePost.blade.php
+++ resources/views/social/singlePost.blade.php
@@ -21,89 +21,11 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">User Posts</div>
+                    <div class="panel-heading">Post Page</div>
 
                     <div class="panel-body">
 
-                            <div class="post-block" id="post{{ $post->id }}">
-                                <div class="post-content-body">
-                                    {{ $post->content }}
-                                </div>
-                                <div class="post-content-details">
-                                    <div class="post-content-user">
-                                        {{ $post->user->name }}
-                                    </div>
-                                    <div class="post-content-date">
-                                        {{ $post->created_at }}
-                                    </div>
-                                    <div class="post-content-comments-count"> Comments <span class="badge">{{ count($post->comments) }}</span> </div>
-                                </div>
-                                <div class="comments-block">
-                                    @foreach($post->comments as $comment)
-                                        <div class="comment-block" id="comment{{ $comment->id }}">
-                                            <div class="comment-content-body">
-                                                {{ $comment->text }}
-                                            </div>
-                                            <div class="comment-content-details">
-                                                <div class="post-content-user">
-                                                    {{ $comment->user->name }}
-                                                </div>
-                                                <div class="comment-content-date">
-                                                    {{ $comment->created_at }}
-                                                </div>
-                                                <div class="comment-content-replyes-count"> Replies <span class="badge">{{ count($comment->replies) }}</span>
-                                                    <a class="show-reply-form">Add Reply</a>
-                                                </div>
-                                            </div>
-                                            <div class="replies-block">
-                                                @foreach($comment->replies as $reply)
-                                                    <div class="reply-block" id="comment{{ $reply->id }}">
-                                                        <div class="comment-content-body">
-                                                            {{ $reply->text }}
-                                                        </div>
-                                                        <div class="comment-content-details">
-                                                            <div class="post-content-user">
-                                                                {{ $reply->user->name }}
-                                                            </div>
-                                                            <div class="comment-content-date">
-                                                                {{ $reply->created_at }}
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                @endforeach
-                                                <div class="reply-form">
-                                                    <form action="{{ URL::to('comments') }}" method="post">
-                                                        {{ csrf_field() }}
-                                                        <textarea name="text" placeholder="Your Reply Here" required></textarea>
-                                                        <input type="hidden" name="commentable_type" value="App\Entities\Comment" />
-                                                        <input type="hidden" name="commentable_id" value="{{ $comment->id }}" />
-                                                        <button class="btn btn-info btn-sm" > Add Reply</button>
-                                                    </form>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                        <hr>
-                                    @endforeach
-
-                                        <div class="comment-form">
-                                            <form action="{{ URL::to('comments') }}" method="post">
-                                                {{ csrf_field() }}
-                                                <textarea name="text" placeholder="Your Comment Here" required></textarea>
-                                                <input type="hidden" name="commentable_type" value="App\Entities\Post" />
-                                                <input type="hidden" name="commentable_id" value="{{ $post->id }}" />
-                                                <button class="btn btn-info btn-sm" > Add Comment</button>
-                                            </form>
-                                        </div>
-
-
-                                </div>
-
-
-
-
-                            </div>
+                        @include('social.post-block', ['post' => $post])
 
                     </div>
                 </div>
--- tests/PostTest.php
+++ tests/PostTest.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Comment;
-use App\Post;
+use App\Entities\Comment;
+use App\Entities\Post;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Auth;
 
 class PostTest extends TestCase
 {
@@ -19,24 +20,69 @@ class PostTest extends TestCase
     /** @test */
     public function each_post_can_have_comments()
     {
+        $user_id = 1;
+
         $post = new Post();
-        $post->content = 'My Post Content';
+        $post->content = 'My Test Post Content';
+        $post->post_type = 'text';
+        $post->user_id = $user_id;
         $post->save();
+
 
         $comment1 = new Comment();
         $comment1->text = 'my comment text';
-        $comment1->post_id = $post->id;
+        $comment1->commentable_type = 'App\Entities\Post';
+        $comment1->commentable_id = $post->id;
+        $comment1->user_id = $user_id;
         $comment1->save();
+
 
         $comment2 = new Comment();
         $comment2->text = 'my comment text2';
-        $comment2->post_id = $post->id;
+        $comment2->commentable_type = 'App\Entities\Post';
+        $comment2->commentable_id = $post->id;
+        $comment2->user_id = $user_id;
         $comment2->save();
 
-        $post_comments = $post->Comments();
+        $post_comments = $post->comments;
         $this->assertEquals(2, count($post_comments));
 
 
 
     }
+
+    /** @test */
+    public function each_post_can_have_comments_by_repository()
+    {
+        $user_id = 1;
+
+        $postRepository = app('\App\Repositories\PostRepositoryEloquent');
+        $attrs['user_id'] =  $user_id;
+        $attrs['content'] =  'My Test Post Content00';
+        $attrs['post_type'] =  'text';
+        $post = $postRepository->create($attrs);
+
+
+        $commentRepository = app('\App\Repositories\CommentRepositoryEloquent');
+        $comment1['user_id'] =  $user_id;
+        $comment1['text'] =  'my comment text10';
+        $comment1['commentable_type'] =  'App\Entities\Post';
+        $comment1['commentable_id'] =  $post->id;
+        $commentRepository->create($comment1);
+
+
+        $comment2['user_id'] =  $user_id;
+        $comment2['text'] =  'my comment text11';
+        $comment2['commentable_type'] =  'App\Entities\Post';
+        $comment2['commentable_id'] =  $post->id;
+        $commentRepository->create($comment2);
+
+        $post_comments = $post->comments;
+        $this->assertEquals(2, count($post_comments));
+
+
+
+    }
+
+
 }
